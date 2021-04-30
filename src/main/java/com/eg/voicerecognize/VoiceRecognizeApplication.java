7fef40d289596ea29183c02e19d43782b5cf16cb
bq <email>
2021-04-30 14:02:27 +0800

    Initial commit

---
+++ src/main/java/com/eg/voicerecognize/VoiceRecognizeApplication.java
@@ -0,0 +1,13 @@
+package com.eg.voicerecognize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceRecognizeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoiceRecognizeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eg/voicerecognize/VoiceRecognizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eg.voicerecognize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoiceRecognizeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
